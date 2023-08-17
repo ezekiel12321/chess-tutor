25a9c45d40b54eee0111fb7f67d7e83a77e36ceb
Ezekiel Norman <norman.zeke11@gmail.com>
2023-08-17 15:55:58 -0700

    changes

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  
   plugins: [react()],
-})
+  build: {
+    // Output directory for assets, relative to the build output directory (dist by default)
+    assetsDir: 'assets',
+  },
+  base: '/', // Base public path when serving the project
+});
